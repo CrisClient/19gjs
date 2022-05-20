bce37b486c236ea2ff0bcdfabf7077e15cd1b2f2
CrisClient <oyeecriswg@gmail.com>
2022-05-20 17:28:26 -0500

    ADDED FILES OF CLASS TWO

---
+++ js/conditionals.js
@@ -0,0 +1,54 @@
+/**
+ * Condicionales
+ */
+
+// Operadores de asignacion
+
+let total = 0
+let a = 4
+let b = 5
+
+total = a+b
+
+// console.log(total)
+
+// total = total + 5
+// total += 5
+
+// total = total - 3
+// total -= 3
+
+// total = total * 2
+// total *= 2
+
+// total = total / 3
+// total /= 3
+
+// total = total % 3
+// total %= 3
+
+// numero % 2 === 0 ? console.log('Es par') : console.log('es impar')
+// numero % 2 === 0 
+//     ? 
+//     console.log(' es par')
+//     : 
+//     console.log('es impar')
+
+// let esPar = numero % 2 === 0 ? true : false
+// console.log(`El numero ${numero} es ${esPar === true ? 'par' : 'impar' } `)
+
+// switch
+let age = parseInt( prompt('cual es tu edad', 17) )
+let isAge = age >= 18 ? true : false
+
+switch (isAge) {
+    case true:
+        console.log('Eres Mayor de edad')
+        break
+    case false: 
+        console.log('eres Menor de edad')
+        break
+    default:
+        console.log('No es una edad valida')
+        break
+}
---
+++ js/string_methods.js
@@ -0,0 +1,8 @@
+
+// Metodos de string
+// funciones
+// str.nombreMetodo(p1,p2,pN)
+// .toUpperCase()
+// .toLowerCase()
+// .substring(start, end)
+// .slice(start, end)
